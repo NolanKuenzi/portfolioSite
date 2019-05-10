814490607a4d9be72e45f79b27959e03938329cc
Nolan Kuenzi <mr.nolank@gmail.com>
2019-05-09 22:41:42 -0700

    Netlify update 5

--- src/js/about.js
+++ src/js/about.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => (
   <div id="aboutSection">
     <div>
-      <img id="aboutImg" src="./dist/images/Pixel_Art.jpg" alt="" />
+      <img id="aboutImg" src="../../dist/images/Pixel_Art.jpg?nf_resize=fit&w=1192&h=672" alt="" />
     </div>
     <div id="aboutText">
       <h3>About Me:</h3>
