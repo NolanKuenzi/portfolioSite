bccfbb5e7b83589882f324d218fd47540761e075
Nolan Kuenzi <mr.nolank@gmail.com>
2019-08-29 00:45:51 -0700

    config update

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -5,7 +5,7 @@ const merge = require("webpack-merge");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+//const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const APP_DIR = path.resolve(__dirname, '../src');
 
@@ -42,7 +42,7 @@ module.exports = env => {
             'process.env.VERSION': JSON.stringify(env.VERSION),
             'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
           }),
-           new CopyWebpackPlugin([ { from: 'src/static' } ]),
+          // new CopyWebpackPlugin([ { from: 'src/static' } ]),
         ],
         output: {
         filename: '[name].bundle.js',
